1b1fca07a1a8fae2745b892cc00a597180a5e39b
Glitch (lucky-scintillating-duchess) <none>
2021-11-17 23:05:41 +0000

    😼🐒 Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 
+
+ let computerNumber = 0;
+
+$(".play").click(function() {
+
+    let userInput = $(".input").val();
+    $(".userChoice").text(userInput);
+
+  let computerNumber;
+computerNumber = Math.random() * 4;
+computerNumber = Math.ceil(computerNumber);
+    $(".computerChoice").text(computerNumber);
+
+});
+
