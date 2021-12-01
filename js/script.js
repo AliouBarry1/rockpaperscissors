dd3d6679cb3138f3d082e046b4fea25db6ba5c32
Glitch (rock-paper-scissor-poject) <none>
2021-12-01 23:10:58 +0000

    🛳🚡 Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -1,16 +1,37 @@
 
 
  let computerNumber = 0;
+let userInput = "";
+let computerChoice ="";
+
 
 $(".play").click(function() {
 
-    let userInput = $(".input").val();
+    userInput = $(".input").val();
+  if(userInput !=="rock" && userInput !=="scissors" && userInput !=="paper"){
+  $(".userChoice").text("Try Again")
+}
+  else{ 
     $(".userChoice").text(userInput);
+      }
 
-  let computerNumber;
+ computerNumber;
 computerNumber = Math.random() * 4;
 computerNumber = Math.ceil(computerNumber);
-    $(".computerChoice").text(computerNumber);
+
+  
+computerChoice;
+if(computerNumber === 1){
+   computerChoice = "rock"
+  $(".computerChoice").text( computerChoice);
+}
+else if(computerNumber === 2){
+   computerChoice = "paper"
+  $(".computerChoice").text( computerChoice);
+}
+else if(computerNumber === 3){
+   computerChoice = "scissors "
+  $(".computerChoice").text( computerChoice);
+}
 
 });
-
