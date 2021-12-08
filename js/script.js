b17f5556781dcbb4f72c7d2c336a3daaed769d7b
Glitch (rock-paper-scissor-poject) <none>
2021-12-08 23:22:16 +0000

    🍘📅 Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -1,37 +1,49 @@
-
-
- let computerNumber = 0;
+let computerNumber = 0;
 let userInput = "";
-let computerChoice ="";
-
+let computerChoice = "";
 
 $(".play").click(function() {
-
-    userInput = $(".input").val();
-  if(userInput !=="rock" && userInput !=="scissors" && userInput !=="paper"){
-  $(".userChoice").text("Try Again")
-}
-  else{ 
+  userInput = $(".input").val();
+  if (
+    userInput !== "rock" &&
+    userInput !== "scissors" &&
+    userInput !== "paper"
+  ) {
+    $(".userChoice").text("Try Again");
+  } else {
     $(".userChoice").text(userInput);
-      }
+  }
 
- computerNumber;
-computerNumber = Math.random() * 4;
-computerNumber = Math.ceil(computerNumber);
+  computerNumber;
+  computerNumber = Math.random() * 4;
+  computerNumber = Math.ceil(computerNumber);
 
-  
-computerChoice;
-if(computerNumber === 1){
-   computerChoice = "rock"
-  $(".computerChoice").text( computerChoice);
-}
-else if(computerNumber === 2){
-   computerChoice = "paper"
-  $(".computerChoice").text( computerChoice);
-}
-else if(computerNumber === 3){
-   computerChoice = "scissors "
-  $(".computerChoice").text( computerChoice);
-}
+  computerChoice;
+  if (computerNumber === 1) {
+    computerChoice = "rock";
+    $(".computerChoice").text(computerChoice);
+  } else if (computerNumber === 2) {
+    computerChoice = "paper";
+    $(".computerChoice").text(computerChoice);
+  } else if (computerNumber === 3) {
+    computerChoice = "scissors";
+    $(".computerChoice").text(computerChoice);
+  }
 
+  if (userInput === computerChoice) {
+    $(".result").text("Tie");
+  } else if (
+    (userInput === "rock" && computerChoice === "scissors") ||
+    (userInput === "paper" && computerChoice === "rock") ||
+    (userInput === "scissors" && computerChoice === "paper")
+  ) {
+    $(".result").text("User Won!! :)");
+  } else if (
+    (userInput === "scissors" && computerChoice === "rock") ||
+    (userInput === "rock" && computerChoice === "paper") ||
+    (userInput === "paper" && computerChoice === "scissors")
+  ) {
+    $(".result").text("Computer Won!! :(");
+  }
+  $("input").val(" ");
 });
